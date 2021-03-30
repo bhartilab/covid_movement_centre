70678f6daa816943bc22f8ec9fcd40ee2eefbb87
cfaustus <christina.faust@gmail.com>
2021-03-30 10:49:24 +0100

    fixed local output for visualizing

--- radiance_cleaning.R
+++ radiance_cleaning.R
@@ -1,43 +1,44 @@
+#######################
+# Radiance data processing
+######################
+# libraries
+library(raster)
 
+######################
+# directory locations (some files outside of github due to size)
+out_dir = "/Users/cfaust/Documents/workspace/pageocov_large_files/output"
+shp_dir = '/Users/cfaust/Documents/workspace/pageocov_large_files/shapefiles'
+large_dir = '/Users/cfaust/Documents/workspace/pageocov_large_files'
 
-# importing geotiffs 
+# importing geotiffs
+# these can be generated from raw data using 'radiance_*.py'  scripts
 daily_files = list.files(file.path(large_dir, 'confident_clear_only/vnp46a1'), pattern = ".tif", full.names = TRUE)
 daily_data = lapply(daily_files, raster) # raster list
 
+#####################
+# population centres - identified with population densities from WorldPop
 # bellefonte and state college
 centre_focal = raster(file.path(shp_dir, "bellefonte_statecollege_raster.tif")) 
-centre_focal2 = resample(centre_focal, daily_data[[1]])
+centre_focal2 = resample(centre_focal, daily_data[[1]], method = 'bilinear') #
+bellefonte = calc(centre_focal2,
+                  fun=function(x){ x[x >  1.1] <- NA ; return(x)} )
+state = calc(centre_focal2,
+             fun=function(x){ x[x <  1.1] <- NA ; return(x)} )
 
 # cropping, masking and stacking
 daily_masked = lapply(daily_data, raster::crop, centre_focal2) 
 daily_masked = lapply(daily_masked, raster::mask, centre_focal2) 
 raw_maskedst = stack(daily_masked)
 
-# phase - year summaries 
-target_phases = c('red_2016','yellow_2016', 'green_2016',
-                  'red_2017','yellow_2017', 'green_2017',
-                  'red_2018','yellow_2018', 'green_2018',
-                  'red_2019','yellow_2019', 'green_2019',
-                  'red_2020','yellow_2020', 'green_2020')
 
-stacked_raw_mean = stack()
-for (i in 1: length(target_phases)){
-  #i = 1
-  x = calc(raster::subset(raw_maskedst, which(daily_df$phase_yr2 %in% target_phases[i])), mean, na.rm = TRUE)
-  stacked_raw_mean = stack(stacked_raw_mean, x)
-}
-names(stacked_raw_mean) = target_phases
+bellefonte_stack = raster::mask(raw_maskedst, bellefonte)
+state_stack = raster::mask(raw_maskedst, state)
 
-bellefonte = calc(centre_focal2,
-                  fun=function(x){ x[x >  1.1] <- NA ; return(x)} )
-bellefonte_stack = raster::mask(stacked_raw_mean, bellefonte)
+writeRaster(raw_maskedst,file.path(out_dir,"daily_stacked_wmoon.grd"), format="raster")
+writeRaster(state_stack,file.path(out_dir,"state_daily_stack_wmoon.grd"), format="raster")
+writeRaster(bellefonte_stack,file.path(out_dir,"bellefonte_daily_stack_wmoon.grd"), format="raster")
 
-state = calc(centre_focal2,
-             fun=function(x){ x[x <  1.1] <- NA ; return(x)} )
-state_stack = raster::mask(stacked_raw_mean, state)
 
-writeRaster(state_stack,file.path(out_dir,"bellefonte_mean_phases_womoon.grd"), format="raster")
-writeRaster(bellefonte_stack,file.path(out_dir,"bellefonte_mean_phases_womoon.grd"), format="raster")
 
 quarts <- c(0,0.25, 0.5, 0.75,1)
 p_names <- purrr::map_chr(quarts, ~paste0(.x*100, "%"))
@@ -46,8 +47,7 @@ p_funs <- purrr::map(p, ~partial(quartile, probs = .x, na.rm = TRUE)) %>%
 quartiles_df = summary_df %>% 
   group_by(phase_year, area) %>% 
   summarize_at(vars(radiance), funs(!!!p_funs))
-write.csv(quartiles_df,'output/quartiles.csv')
-quarts = read.csv('output/quartiles_annotated.csv')
+
 
 #adjusting dataframe 
 quarts$year = gsub(".*_", "", quarts$phase_year)
@@ -61,20 +61,8 @@ quarts$quartile = factor(quarts$quartile,
 quarts$area = as.factor(quarts$area)
 quarts$area = factor(quarts$area, levels = c('state college', 'bellefonte'))
 
-bellefonte_df = as.data.frame(rasterToPoints(bellefonte_stack))
-state_df = as.data.frame(rasterToPoints(state_stack))
+write.csv(quartiles_df,'output/quartiles.csv')
+quarts = read.csv('output/quartiles_annotated.csv')
 
-bellefonte_long <- melt(bellefonte_df, id.vars=c("x", "y"),
-                        variable.name="phase_year", value.name="radiance")
-state_long <- melt(state_df, id.vars=c("x", "y"),
-                   variable.name="phase_year", value.name="radiance")
-bellefonte_long$area = 'bellefonte'
-state_long$area = 'state college'
-summary_df = rbind(state_long, bellefonte_long)
-summary_df$year = gsub(".*_", "", summary_df$phase_year)
-summary_df$year_fac = as.factor(summary_df$year)
-summary_df$phase = gsub("_.*", "", summary_df$phase_year)
-summary_df$phase = as.factor(summary_df$phase)
-summary_df$phase = factor(summary_df$phase, 
-                          levels =c('red', 'yellow','green'))
+
 
